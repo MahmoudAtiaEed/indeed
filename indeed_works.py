f8893ef3d4b564c63c2da409b8fbb5360764e703
MahmoudAtiaEed <103922786+MahmoudAtiaEed@users.noreply.github.com>
2022-04-27 15:17:37 +0300

    oop

--- indeed_works.py
+++ indeed_works.py
@@ -4,7 +4,7 @@ import pandas as pd
 import time
 import streamlit as st
 
-url = st.text_input("paste an indeed url")
+
 
 class indeed:
     df = pd.DataFrame({"jop title": [],
@@ -34,20 +34,5 @@ class indeed:
            
     def get_jops(self):
         return(self.df)
-    def jops_num(self):
-        print(len(self.df.index))
-    def jops_excel(self):
-        try:
-            desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')  # finding desktop path windows
-        except:
-            desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')  # or finding it in mac
-        path = desktop + "\indeed jops"  # path to creating the direktory
+  
 
-        with open(path, "w")as f:
-            self.df.to_excel(f)
-
-b = st.button("see data frame")
-if b:
-    jop = indeed(url)
-
-    st.table(jop.get_jops())
