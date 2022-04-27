7534f503ab69e9203aa29500c93442179705b138
MahmoudAtiaEed <103922786+MahmoudAtiaEed@users.noreply.github.com>
2022-04-27 15:25:36 +0300

    look

--- indeed_works.py
+++ indeed_works.py
@@ -34,5 +34,10 @@ class indeed:
            
     def get_jops(self):
         return(self.df)
-  
 
+url = st.text_input("paste an indeed url")
+b = st.button("see data frame")
+if b:
+    jop = indeed(url)
+
+    st.table(jop.get_jops())
